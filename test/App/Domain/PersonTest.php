1d59b459c9308eb2569158dfdd0e661aef7d0d8c
Rafael Bernard Araújo <rafael.araujo@initd.info>
2021-12-13 12:56:15 -0300

    Add Person test

---
+++ test/App/Domain/PersonTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test\App\Domain;
+
+use App\Domain\Age;
+use App\Domain\Person;
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
+{
+    private const NAME = 'John Doe';
+    private const AGE = 18;
+
+    public function testCanInstantiate()
+    {
+        $class = new Person(self::NAME, new Age(self::AGE));
+
+        self::assertInstanceOf(Person::class, $class);
+    }
+
+    public function testCanReadProperties()
+    {
+        $person = new Person(self::NAME, new Age(self::AGE));
+
+        self::assertEquals(self::NAME, $person->name());
+        self::assertEquals(self::AGE, $person->age()->value());
+    }
+}
